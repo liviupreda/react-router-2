491918501e5a737246904016fb6d9e170a818a04
Liviu Preda <liviupreda@gmail.com>
2019-11-29 08:56:45 +0200

    react router patterns initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Food from "./Food";
+import Meal from "./Meal";
+import Navbar from "./Navbar";
+import FoodSearch from "./FoodSearch";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        {/* THIS VERSION USING COMPONENT WORKS AND IS SIMPLER */}
+        {/* BUT IT WILL NOT WORK IF YOU NEED TO PASS ADDITIONAL PROPS*/}
+        {/* <Route exact path='/food/:name' scomponent={Food} /> */}
+
+        {/* THIS VERSION USING RENDER IS LESS CLEAN BUT MORE EXPLICIT */}
+        {/* AND YOU CAN PASS IN YOUR OWN ADDITIONAL PROPS */}
+        <Navbar />
+        <Switch>
+          <Route
+            exact
+            path='/food/:name'
+            render={routeProps => <Food {...routeProps} />}
+          />
+          <Route
+            exact
+            path='/food/:foodName/drink/:drinkName'
+            component={Meal}
+          />
+          {/* <Route
+            exact
+            path='/'
+            render={routeProps => <FoodSearch {...routeProps}/>}
+          /> */}
+          <Route exact path='/' component={FoodSearch} />
+          <Route render={() => <h1>ERROR NOT FOUND!!!</h1>} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
