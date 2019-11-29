3c4fafa7f6e43561505b7ed1e1d9a8b0bc90ebc2
Liviu Preda <liviupreda@gmail.com>
2019-11-29 10:13:24 +0200

    use Unsplash source API for images

--- src/Food.js
+++ src/Food.js
@@ -3,18 +3,11 @@ import { Redirect } from "react-router-dom";
 import "./Food.css";
 class Food extends Component {
   render() {
-    const name = this.props.match.params.name;
-    const url = `https://source.unsplash.com/1600x900/?${name}`;
+    const url = `https://source.unsplash.com/1600x900/?${this.props.name}`;
     return (
-      <div className='Food'>
-        {/\d/.test(name) ? (
-          <Redirect to='/' />
-        ) : (
-          <div>
-            <h1>I love to eat {name}</h1>
-            <img src={url} alt={name} />
-          </div>
-        )}
+      <div className="Food">
+        <h1>I love to eat {this.props.name}</h1>
+        <img src={url} alt={this.props.name} />
       </div>
     );
   }
